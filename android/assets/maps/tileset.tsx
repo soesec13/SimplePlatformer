c52dea7fc74a8d4309fd61589f04ef0bc941f350
Sebi <sebi.wurst@gmail.com>
2017-06-21 20:45:19 +0200

    [Git Fix] Added Assets

---
+++ android/assets/maps/tileset.tsx
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Basic" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="generic_platformer_tiles.png" width="1024" height="768"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="186"/>
+  <terrain name="New Terrain" tile="186"/>
+  <terrain name="New Terrain" tile="186"/>
+ </terraintypes>
+ <tile id="54" terrain=",,,0"/>
+ <tile id="55" terrain=",,0,0"/>
+ <tile id="56" terrain=",,0,0"/>
+ <tile id="57" terrain=",,0,0"/>
+ <tile id="58" terrain=",,0,0"/>
+ <tile id="59" terrain=",,0,"/>
+ <tile id="87" terrain=",0,,0"/>
+ <tile id="88" terrain="0,0,0,0"/>
+ <tile id="89" terrain="0,0,0,0"/>
+ <tile id="90" terrain="0,,0,"/>
+ <tile id="119" terrain=",0,,"/>
+ <tile id="120" terrain="0,0,,0"/>
+ <tile id="121" terrain="0,0,0,"/>
+ <tile id="122" terrain="0,,,"/>
+ <tile id="152" terrain=",0,,"/>
+ <tile id="153" terrain="0,,,"/>
+ <tile id="182" terrain=",,,1"/>
+ <tile id="183" terrain=",,1,1"/>
+ <tile id="184" terrain=",,1,1"/>
+ <tile id="185" terrain=",,1,1"/>
+ <tile id="186" terrain=",,1,1"/>
+ <tile id="187" terrain=",,1,"/>
+ <tile id="215" terrain=",1,,1"/>
+ <tile id="216" terrain="1,1,1,1"/>
+ <tile id="217" terrain="1,1,1,1"/>
+ <tile id="218" terrain="1,,1,"/>
+ <tile id="247" terrain=",1,,"/>
+ <tile id="248" terrain="1,1,,1"/>
+ <tile id="249" terrain="1,1,1,"/>
+ <tile id="250" terrain="1,,,"/>
+ <tile id="280" terrain=",1,,"/>
+ <tile id="281" terrain="1,,,"/>
+ <tile id="597" terrain=",,,2"/>
+ <tile id="598" terrain=",,2,2"/>
+ <tile id="599" terrain=",,2,2"/>
+ <tile id="600" terrain=",,2,"/>
+ <tile id="629" terrain=",2,,2"/>
+ <tile id="630" terrain="2,2,2,2"/>
+ <tile id="631" terrain="2,2,2,2"/>
+ <tile id="632" terrain="2,,2,2"/>
+ <tile id="633" terrain=",,2,"/>
+</tileset>
